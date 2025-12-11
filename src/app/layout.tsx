6244c0ad4fac5e364c470d8121af92205ce3f6ba
Angelina Lavely <lavelyangelina26@gmail.com>
2025-12-11 07:21:23 +0000

    why cant i interact with the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,8 @@ export default function RootLayout({
               </SidebarInset>
             </div>
           </SidebarProvider>
+          <Toaster />
         </HabitProvider>
-        <Toaster />
       </body>
     </html>
   );
